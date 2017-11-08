55e18c26d5d9b66bfa7c2900a4525ef28674d70a
Akshay Kumar Gupta <akshay.akgupta@gmail.com>
2017-11-08 16:45:27 +0530

    Updated

--- convert.py
+++ convert.py
@@ -1,9 +1,12 @@
-f1 = open('aido99_all.txt', 'r')
-f2 = open('ca.txt', 'r')
-f3 = open('ci.txt', 'r')
+import sys
+
+f1 = open(sys.argv[1], 'r')
+f2 = open(sys.argv[2], 'r')
+f3 = open(sys.argv[3], 'r')
 
 active = {}
 inactive = {}
+test = {}
 
 for line in f2:
 	active[line.strip()] = []
@@ -17,6 +20,7 @@ def nodeLabel(node):
 		labels[node] = len(labels)
 	return labels[node]
 
+gcount = 0
 while 1:
 	line = f1.readline().strip()
 	if line == '':
@@ -37,47 +41,91 @@ while 1:
 	if (graphID in active):
 		active[graphID].append(nodes)
 		active[graphID].append(edges)
+		active[graphID].append(gcount)
 
 	if (graphID in inactive):
 		inactive[graphID].append(nodes)
 		inactive[graphID].append(edges)
+		inactive[graphID].append(gcount)
+
+	gcount += 1
 
 f1.close()
 f2.close()
 f3.close()
 
-f4 = open('aids_data.txt', 'w')
-f5 = open('aids_class.txt', 'w')
+ftest = open(sys.argv[4], 'r')
+
+while 1:
+	line = ftest.readline().strip()
+	if line == '':
+		break
+
+	graphID = line[1:]
+
+	nodes = []
+	numNodes = int(ftest.readline().strip())
+	for i in xrange(numNodes):
+		nodes.append(nodeLabel(ftest.readline().strip()))
+
+	edges = []
+	numEdges = int(ftest.readline().strip())
+	for i in xrange(numEdges):
+		edges.append(ftest.readline().strip())
+
+	test[graphID] = []
+	test[graphID].append(nodes)
+	test[graphID].append(edges)
+
+ftest.close()
+
+f4 = open('train_pos.txt', 'w')
+f5 = open('train_neg.txt', 'w')
+f6 = open('test_graphs.txt', 'w')
 
 for key in active:
 	if active[key] == []:
 		continue
 
-	f5.write("1\n")
-	f4.write("t # " + key + "\n")
-
 	graph = active[key]
 
+	f4.write("t # " + key + " " + str(graph[2]) + "\n")
+
 	for i in xrange(len(graph[0])):
-		 f4.write("v " + str(i) + " " + str(graph[0][i]) + "\n")
+		f4.write("v " + str(i) + " " + str(graph[0][i]) + "\n")
 
 	for i in xrange(len(graph[1])):
-		 f4.write("e " + graph[1][i] + "\n")
+		f4.write("e " + graph[1][i] + "\n")
 
 for key in inactive:
 	if inactive[key] == []:
 		continue
 
-	f5.write("0\n")
-	f4.write("t # " + key + "\n")
+	f5.write("t # " + key + " " + str(graph[2]) + "\n")
 
 	graph = inactive[key]
 
 	for i in xrange(len(graph[0])):
-		 f4.write("v " + str(i) + " " + str(graph[0][i]) + "\n")
+		f5.write("v " + str(i) + " " + str(graph[0][i]) + "\n")
 
 	for i in xrange(len(graph[1])):
-		 f4.write("e " + graph[1][i] + "\n")
+		f5.write("e " + graph[1][i] + "\n")
+
+
+for key in test:
+	if test[key] == []:
+		continue
+
+	f6.write("t # " + key + "\n")
+
+	graph = test[key]
+
+	for i in xrange(len(graph[0])):
+		f6.write("v " + str(i) + " " + str(graph[0][i]) + "\n")
+
+	for i in xrange(len(graph[1])):
+		f6.write("e " + graph[1][i] + "\n")
 
 f4.close()
 f5.close()
+f6.close()
--- subgraphiso.cpp
+++ subgraphiso.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include <map>
 
 // VFLib includes
 #include <argraph.h>
@@ -69,6 +70,9 @@ bool subgraphiso(ARGraph<Atom, Bond> *g1, ARGraph<Atom, Bond> *g2) {
 }
 
 class DBGraph {
+
+public:
+
 	ARGraph<Atom, Bond>* g;
 
 	int id;
@@ -77,7 +81,6 @@ class DBGraph {
 
 	bool feature;
 
-public:
 	DBGraph (ARGEdit *ed, int id, int type, int support, bool gaston) {
 		this->g = new ARGraph<Atom, Bond>(ed);
 
@@ -96,20 +99,24 @@ public:
 	}
 };
 
-void readFile(string filename, bool gaston, int type, vector<DBGraph*>& db) {
+void readFile(string filename, bool gaston, int type, vector<DBGraph*>& db, bool train, map<int, DBGraph*>& ordered_train) {
 	ifstream f(filename);
 
 	char temp;
 	f >> temp;
 	while (1) {
-		int id, support = 0;
+		int id, support = 0, gcount;
 
 		if (gaston) {
 			f >> support;
 			f >> temp;
 		}
-		f >> temp;
+		if (!gaston)
+			f >> temp;
 		f >> id;
+		if (train) {
+			f >> gcount;
+		}
 
 		f >> temp;
 		ARGEdit ed;
@@ -136,14 +143,94 @@ void readFile(string filename, bool gaston, int type, vector<DBGraph*>& db) {
 		}
 
 		db.push_back(new DBGraph(&ed, id, type, support, gaston));
+		if(train) {
+			ordered_train.insert(pair<int, DBGraph*>(gcount, db.back()));
+		}
 
 		if ((int)temp == 0)
 			break;
 	}
+	f.close();
+}
+
+void check_significance(vector<DBGraph*>& subgraphs, vector<DBGraph*>& min_class_graphs, double alpha) {
+	int out_trial = 0;
+	for(int i = 14; i < subgraphs.size(); i++) {
+		cout << "Out_trial: " << out_trial << "\n";
+		int in_trial = 0;
+		int min_count = 0;
+		for(int j = 0; j < min_class_graphs.size(); j++) {
+			cout << (min_class_graphs[j]->g)->NodeCount() << "\n";
+			cout << (subgraphs[i]->g)->NodeCount() << "\n";
+			int is_subgraph = subgraphiso(subgraphs[i]->g, min_class_graphs[j]->g);
+			cout << is_subgraph << "\n";
+			if(is_subgraph)
+				min_count++;
+			cout << "In_trial: " << in_trial << "\n";
+			in_trial++;
+		}
+		double ratio = ((double) subgraphs[i]->support)/((double) subgraphs[i]->support + min_count);
+		if(ratio > alpha) {
+			subgraphs[i]->feature = true;
+			cout << "Min_count: " << min_count << "\n";
+		}
+		out_trial++;
+	}
+}
+
+void generate_features(map<int, DBGraph*>& train, vector<DBGraph*>& gaston_pos, vector<DBGraph*>& gaston_neg, vector<DBGraph*>& test) {
+	ofstream ftrain("train.txt");
+	for(int j = 0; j < train.size(); j++) {
+		ftrain << train[j]->type;
+		for(int i = 0; i < gaston_pos.size(); i++) {
+			if(!(gaston_pos[i]->feature))
+				continue;
+			if(subgraphiso(gaston_pos[i]->g, train[j]->g))
+				ftrain << gaston_pos[i]->id << ":1 ";
+			else
+				ftrain << gaston_pos[i]->id << ":0 ";
+		}
+		for(int i = 0; i < gaston_neg.size(); i++) {
+			if(!(gaston_neg[i]->feature))
+				continue;
+			if(subgraphiso(gaston_neg[i]->g, train[j]->g))
+				ftrain << gaston_neg[i]->id << ":1 ";
+			else
+				ftrain << gaston_neg[i]->id << ":0 ";
+		}
+		ftrain << "\n";
+	}
+	ftrain.close();
+	ofstream ftest("test.txt");
+	for(int j = 0; j < train.size(); j++) {
+		for(int i = 0; i < gaston_pos.size(); i++) {
+			if(!(gaston_pos[i]->feature))
+				continue;
+			if(subgraphiso(gaston_pos[i]->g, train[j]->g))
+				cout << gaston_pos[i]->id << ":1 ";
+			else
+				cout << gaston_pos[i]->id << ":0 ";
+		}
+		for(int i = 0; i < gaston_neg.size(); i++) {
+			if(!(gaston_neg[i]->feature))
+				continue;
+			if(subgraphiso(gaston_neg[i]->g, train[j]->g))
+				ftrain << gaston_neg[i]->id << ":1 ";
+			else
+				ftrain << gaston_neg[i]->id << ":0 ";
+		}
+	}
+	ftest.close();
 }
 
 int main() {
-	vector<DBGraph*> db;
-	readFile("aids_data.txt", false, 0, db);
+	vector<DBGraph*> train_pos, train_neg, gaston_pos, gaston_neg, test;
+	map<int, DBGraph*> ordered_train;
+	readFile("train_pos.txt", false, 1, train_pos, true, ordered_train);
+	readFile("train_neg.txt", false, -1, train_neg, true, ordered_train);
+	readFile("gaston_pos.txt", true, 1, gaston_pos, false, ordered_train);
+	readFile("gaston_neg.txt", true, -1, gaston_neg, false, ordered_train);
+	readFile("test_graphs.txt", false, 0, test, false, ordered_train);
+	check_significance(gaston_neg, train_pos, 0.8);
 	return 0;
 }
